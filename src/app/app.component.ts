11362d8728b243d996fc553bd526f79070e9680d
Luis Pichardo <lpichardosanchez2@gmail.com>
2022-07-03 02:24:27 +0800

    PRIMER CRUD CON ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud';
+  employeeArray: Employee [] = [
+{ id: 1, name: "Luis",country:"USA"},
+{id: 2, name: "Alfredo",country:"España"},
+{id:3,name: "Angel",country:"Dominicano"}
+];
+
+seleEmployee: Employee = new Employee();
+
+openForEdit(employee: Employee){
+this.seleEmployee = employee;
+}
+
+addOrEdit(){
+
+  if(this.seleEmployee.id === 0){
+    this.seleEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.seleEmployee);
+  }
+  this.seleEmployee = new Employee();
+}
+delete(){
+  if(confirm("Esta seguro de que quiere borrar este registro ?")){
+    this.employeeArray = this.employeeArray.filter(x => x != this.seleEmployee);
+  this.seleEmployee = new Employee();
+  }
+}
 }
